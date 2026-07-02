cdbb28e1963a7122f85fc567ebce63d84a765add
Martina Veschi <allievo_veschi.m@itsumbria.it>
2026-07-02 15:54:24 +0200

    ho modificato una stringa in linea 232 su electronicregisterapp.jsx

--- Frontend/ElectronicRegisterReact/ElectronicRegisterApp.jsx
+++ Frontend/ElectronicRegisterReact/ElectronicRegisterApp.jsx
@@ -229,7 +229,7 @@ function RegisterScreen({ onBack }) {
       <StatusBar barStyle="light-content" backgroundColor={C.primary} />
       <View style={s.authHeader}>
         <Text style={s.authLogo}>🎓</Text>
-        <Text style={s.authTitle}>Registro Elettronico</Text>
+        <Text style={s.authTitle}>Registro Elettronic</Text>
       </View>
       <ScrollView contentContainerStyle={s.authBody}>
         <Card>
