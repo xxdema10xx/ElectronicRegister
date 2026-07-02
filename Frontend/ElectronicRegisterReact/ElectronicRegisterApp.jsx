3cc7740dc0f9452971354cd5360812c088f3ab92
Lorenzo Baracchi <198180881+byeLux@users.noreply.github.com>
2026-07-02 17:11:19 +0200

    connessione possibile al difuori di localhost

--- Frontend/ElectronicRegisterReact/ElectronicRegisterApp.jsx
+++ Frontend/ElectronicRegisterReact/ElectronicRegisterApp.jsx
@@ -17,7 +17,7 @@ import {
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 // ─── CONFIG ──────────────────────────────────────────────────────────────────
-const API_BASE = "http://10.10.54.58:5257/api"; // ← cambia con il tuo URL
+const API_BASE = "https://10.10.54.33:7225/api"; // ← cambia con il tuo URL
 
 // ─── COLORS ──────────────────────────────────────────────────────────────────
 const C = {
