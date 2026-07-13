56206119018de4c8c70353b56cee18b91139704d
Lorenzo Baracchi <198180881+byeLux@users.noreply.github.com>
2026-07-13 11:35:48 +0200

    aggiunto l'update della password

---
+++ Backend/ElectronicRegisterAPI/DTOs/UpdatePasswordDto.cs
@@ -0,0 +1,8 @@
+namespace ElectronicRegisterAPI.DTOs
+{
+    public class UpdatePasswordDto
+    {
+        public string? OldPassword { get; set; }
+        public string? NewPassword { get; set; }
+    }
+}
