1d0037961c8c111c28170be5f6a8198992663dd8
Damiano De  Mattei <allievo_demattei.d@itsumbria.it>
2026-09-25 12:36:56 +0200

    Ripristinato originale

--- Backend/ElectronicRegisterAPI/src/Domain/ElectronicRegisterAPI.Domain/Models/Grade.cs
+++ Backend/ElectronicRegisterAPI/src/Domain/ElectronicRegisterAPI.Domain/Models/Grade.cs
@@ -6,6 +6,6 @@ public class Grade
     public Guid Id { get; set; }
     public Guid StudentId { get; set; }
     public Guid ClassSubjectId { get; set; }
-    public decimal Valu { get; set; }
+    public decimal Value { get; set; }
     public DateOnly Date { get; set; }
 }
