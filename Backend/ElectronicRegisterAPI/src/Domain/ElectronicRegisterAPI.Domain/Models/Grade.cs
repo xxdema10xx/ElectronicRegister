fe4671aa969841e8a3c96a535acff0fb8a4dcb51
Damiano De  Mattei <allievo_demattei.d@itsumbria.it>
2026-09-25 12:36:13 +0200

    Secondo committo

--- Backend/ElectronicRegisterAPI/src/Domain/ElectronicRegisterAPI.Domain/Models/Grade.cs
+++ Backend/ElectronicRegisterAPI/src/Domain/ElectronicRegisterAPI.Domain/Models/Grade.cs
@@ -1,11 +1,11 @@
 // Domain — modello di dominio (Domain/Models/Grade.cs)
 namespace ElectronicRegisterAPI.Domain.Models;
-
+//Sigh heil!
 public class Grade
 {
     public Guid Id { get; set; }
     public Guid StudentId { get; set; }
     public Guid ClassSubjectId { get; set; }
-    public decimal Value { get; set; }
+    public decimal Valu { get; set; }
     public DateOnly Date { get; set; }
 }
